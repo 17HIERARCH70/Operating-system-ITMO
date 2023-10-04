e0b5ad21030474a69b735f3a135be3cda516fdaf
Belyakov Nikita <nikitabelekov@gmail.com>
2023-10-04 19:31:24 +0300

    fixed

--- main.sh
+++ main.sh
@@ -1,103 +1,244 @@
 #!/bin/bash
 
-ForkBombLin(){
-     gcc -o ForkBomb ForkBombLin.c
-            echo "Run..."
-            sudo ./ForkBomb &
-            pid=$!
-            sleep 10
-            kill $pid 
-            rm -f ForkBombLin 
+os=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+work_dir=$PWD
+
+install_programs() 
+{
+	case $os in
+		debian|ubuntu)
+		packages=(
+			["hdparm"]="hdparm"
+			["bonnie++"]="bonnie++"
+			["sysstat"]="sysstat"
+			["fio"]="fio"
+			["lolcat"]="lolcat"
+			["figlet"]="figlet"
+			["xcowsay"]="xcowsay"
+			["cowsay"]="cowsay"
+			["fortune"]="fortune"
+			["boxes"]="boxes"
+			["tmux"]="tmux"
+			["libpam0g-dev"]="libpam0g-dev"
+			["libnfnetlink"]="libnfnetlink"
+			["libnfnetlink-dev"]="libnfnetlink-dev"
+			["libnetfilter-queue-dev"]="libnetfilter-queue-dev"
+			["iptables"]="iptables"
+			["pam-utils"]="pam-utils"
+			["pam"]="pam"
+			["nasm"]="nasm"
+			["base-devel"]="base-devel"
+			["base-devel"]="net-tools"
+			
+		)
+		for package in "${!packages[@]}"; do
+			if ! dpkg -s "${packages[$package]}" > /dev/null 2>&1; then
+				sudo apt-get install -y "${packages[$package]}"
+				
+			else
+				echo "$package уже установлен."
+			fi
+		done
+		sudo apt install boxes
+		;;
+		
+		arch)
+		declare -A packages=(
+			["hdparm"]="hdparm"
+			["bonnie++"]="bonnie++"
+			["sysstat"]="sysstat"
+			["fio"]="fio"
+			["lolcat"]="lolcat"
+			["figlet"]="figlet"
+			["xcowsay"]="xcowsay"
+			["cowsay"]="cowsay"
+			["fortune"]="fortune"
+			["cowfortune"]="cowfortune"
+			["boxes"]="boxes"
+			["tmux"]="tmux"
+			["libpam0g-dev"]="libpam0g-dev"
+			["libnfnetlink"]="libnfnetlink"
+			["libnfnetlink-dev"]="libnfnetlink-dev"
+			["libnetfilter-queue-dev"]="libnetfilter-queue-dev"
+			["iptables"]="iptables"
+			["nasm"]="nasm"
+			["pam-utils"]="pam-utils"
+			["pam"]="pam"
+			["base-devel"]="base-devel"
+			["inetutils"]="inetutils"
+		)
+		for package in "${!packages[@]}"; do
+			if ! pacman -Qs "${packages[$package]}" > /dev/null 2>&1; then
+				sudo pacman -S "${packages[$package]}"
+			else
+				echo "$package уже установлен."
+			fi
+		done
+		
+		if ! pacman -Qs yaourt > /dev/null 2>&1; then
+			current_user=$(logname)
+			sudo pacman -S --needed base-devel git wget yajl
+			cd /tmp
+			git clone https://aur.archlinux.org/package-query.git
+			cd package-query/
+			makepkg -si && cd /tmp/
+			git clone https://aur.archlinux.org/yaourt.git
+			cd yaourt/
+			
+			makepkg -si
+
+		else
+			echo "yaourt уже установлен."
+		fi
+		
+		for package in "${!packages[@]}"; do
+			if ! yaourt -Q "${packages[$package]}" > /dev/null 2>&1; then
+				yaourt -S "${packages[$package]}"
+			else
+				echo "$package уже установлен."
+			fi
+		done
+		
+		sleep 20
+		;;
+		*)
+		echo "Не удалось определить менеджер пакетов для этой операционной системы."
+		exit 1
+		;;
+	esac
 }
 
-ForkBombWin{
-    gcc -o /ForkBomb /ForkBombWin.c
-            /ForkBomb.exe &
-            pid=$! 
-            sleep 10
-            taskkill /F /PID $pid 
-            rm -f ForkBomb.exe 
+remove_programs() 
+{
+	case $os in
+		debian|ubuntu)
+		packages=(
+			["hdparm"]="hdparm"
+			["bonnie++"]="bonnie++"
+			["sysstat"]="sysstat"
+			["fio"]="fio"
+			["lolcat"]="lolcat"
+			["figlet"]="figlet"
+			["xcowsay"]="xcowsay"
+			["cowsay"]="cowsay"
+			["fortune"]="fortune"
+			["boxes"]="boxes"
+			["tmux"]="tmux"
+			["nasm"]="nasm"
+			["libpam0g-dev"]="libpam0g-dev"
+			["libnfnetlink"]="libnfnetlink"
+			["libnfnetlink-dev"]="libnfnetlink-dev"
+			["libnetfilter-queue-dev"]="libnetfilter-queue-dev"
+			["iptables"]="iptables"
+			["pam-utils"]="pam-utils"
+			["pam"]="pam"
+			["base-devel"]="base-devel"
+		)
+		for package in "${!packages[@]}"; do
+			sudo apt-get delete -y "${packages[$package]}"
+		done
+		;;
+		arch)
+		declare -A packages=(
+			["hdparm"]="hdparm"
+			["bonnie++"]="bonnie++"
+			["sysstat"]="sysstat"
+			["fio"]="fio"
+			["lolcat"]="lolcat"
+			["figlet"]="figlet"
+			["xcowsay"]="xcowsay"
+			["cowsay"]="cowsay"
+			["fortune"]="fortune"
+			["boxes"]="boxes"
+			["tmux"]="tmux"
+			["nasm"]="nasm"
+			["libpam0g-dev"]="libpam0g-dev"
+			["libnfnetlink"]="libnfnetlink"
+			["libnfnetlink-dev"]="libnfnetlink-dev"
+			["libnetfilter-queue-dev"]="libnetfilter-queue-dev"
+			["iptables"]="iptables"
+			["pam-utils"]="pam-utils"
+			["pam"]="pam"
+			["base-devel"]="base-devel"
+		)
+		for package in "${!packages[@]}"; do
+			sudo pacman -R "${packages[$package]}"
+		done
+		
+		for package in "${!packages[@]}"; do
+				yaourt -R "${packages[$package]}"
+		done
+		
+		sudo pacman -R --needed base-devel git wget yajl
+		sudo rm -rf /tmp/package-query
+		sudo pacman -R yaourt
+		current_user=$(logname)
+		sudo -u "$current_user" yaourt -R boxes
+		;;
+		*)
+		echo "Не удалось определить менеджер пакетов для этой операционной системы."
+		exit 1
+		;;
+	esac
+	main
 }
 
-MembombLin() {
-     gcc -o MemBomb MemBombLin.c
-            echo "Run..."
-			sudo touch /etc/systemd/system/KillLinux.service
-			sudo chmod 644 /etc/systemd/system/KillLinux.service
+ForkBomb() {
+    echo "ForkBomb" | boxes -d tux -p a1
 
-			sudo tee /etc/systemd/system/KillLinux.service << EOF
-[Unit]
-Description=KillLinux
-After=network.target
+    cd "$BASEDIR/ForkBomb"
 
-[Service]
-ExecStart=$(find $work_dir/MemBomb/ -type f -name "Membomb" -executable -print -quit)
-
-[Install]
-WantedBy=default.target
-EOF
-			sudo systemctl unmask KillLinux.service
-			sudo systemctl daemon-reload
-			sudo systemctl enable KillLinux.service
-			sudo systemctl start KillLinux.service
-            sudo ./MemBomb &
-            pid=$! 
-            sleep 10
-            kill $pid 
-            rm -f MemBombLin 
-}
-
-MemBombWin(){
-    gcc -o MemBombWin MemBombWin.c
-            /MemBombWin.exe &
-            pid=$! 
-            sleep 10
-            taskkill /F /PID $pid 
-            rm -f MemBombWin.exe
-}
-
-
-echo "Добро пожаловать!"
-echo "Список программ:"
-echo "1) ForkBomb"
-echo "2) MemBomb"
-
-read -p "Выберите программу (1/2): " choice
-
-if [ "$choice" == "1" ]; then
-    read -p "Вы уверены, что хотите запустить ForkBomb? (y/n): " confirm
-    if [ "$confirm" == "y" ]; then
+    if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin" ]]; then
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-            # Компиляция и запуск ForkBomb для Linux
-            cd $work_dir/ForkBomb
-            ForkBombLin
-        elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin" ]]; then
-            # Компиляция и запуск ForkBomb для Windows
-            cd $work_dir/ForkBomb
-            ForkBombWin
+            gcc -o main ForkbombLin.c
         else
-            echo "Не удалось определить ОС"
+            gcc -o main ForkbombWin.c
         fi
-    else
-        echo "Запуск ForkBomb отменен"
-    fi
-elif [ "$choice" == "2" ]; then
-    read -p "Вы уверены, что хотите запустить MemBomb? (y/n): " confirm
-    if [ "$confirm" == "y" ]; then
-        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-            # Компиляция и запуск MemBomb для Linux
-            cd $work_dir/MemBomb
-            MembombLin
-        elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin" ]]; then
-            # Компиляция и запуск MemBomb для Windows
-            cd $work_dir/MemBomb
-            MemBombWin
+
+        read -p "Вы уверены?
+        Нажми, чтобы принять - 'y', отклонить - 'n': " response
+
+        if [ "$response" == "y" ]; then
+            echo "Запуск..."
+            sleep 2
+            ./main
+        elif [ "$response" == "n" ]; then
+            echo "Отменяем запуск..."
         else
-            echo "Не удалось определить ОС"
+            echo "Некорректный ввод"
         fi
+
+        read -p "Вы хотите удалить скомпилируемые файлы?(y/n): " response
+
+        if [ "$response" == "y" ]; then
+            find . -iname "main*" -delete
+        elif [ "$response" == "n" ]; then
+            echo "Отменяем удаление"
+        else
+            echo "Некорректный ввод"
+        fi
+
+        cd "$BASEDIR"
+        main
     else
-        echo "Запуск MemBomb отменен"
+        echo "Не поддерживается ОС: $OSTYPE"
     fi
-else
-    echo "Неверный выбор программы"
-fi
+}
+
+
+main() 
+{
+	BASEDIR=$(dirname "$(realpath "$0")")    
+	print_art 
+	read -p "Введи 1-14: " S_Modules
+	case $S_Modules in
+		1) clear; ForkBomb ;;
+        2) clear; MemBomb ;;
+        3) clear; LimPack ;;
+		4) clear; exit;;
+	esac
+	main
+}
+
+install_programs;
+clear; cd $work_dir; main;
